4df58b4c2dddf89150e6b2e6ca025be9e81b7270
Vadim Comanescu <vadim984@gmail.com>
2012-06-26 23:23:25 +0300

    Load object from history

--- lib/repository.rb
+++ lib/repository.rb
@@ -6,10 +6,13 @@ class Repository
   
   def get_by_id aggr_id
     events = @event_store.get_events_for_aggregate aggr_id
+    inventory_item = InventoryItem.new aggr_id, false
+    inventory_item.load_from_history events
+    return inventory_item
   end
   
   def save aggregate_root, expected_version
     @event_store.save_events aggregate_root.id, aggregate_root.changes, expected_version
   end
-	
+
 end
